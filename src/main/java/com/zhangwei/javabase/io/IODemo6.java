6cbb94b49cd822e23df0c188f28472a10de56928
zhangwei1521 <759109612@qq.com>
2020-05-23 15:38:53 +0800

    IO部分API整理

--- src/main/java/com/zhangwei/javabase/io/IODemo6.java
+++ src/main/java/com/zhangwei/javabase/io/IODemo6.java
@@ -1,15 +1,8 @@
 package com.zhangwei.javabase.io;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.CharArrayWriter;
-import java.io.Console;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.nio.CharBuffer;
 
